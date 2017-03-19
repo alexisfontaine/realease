49131479f610b4670ceaf1f9f0bfa59fd07048c0
alexisfontaine <alexis.fontaine@isep.fr>
2017-03-19 20:57:40 +0100

    Use `requestAnimationFrame` to animate

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,17 +5,19 @@ const { length } = releases
 const last       = length - 1
 
 function animate (current = last) {
-	releases.forEach((release, index) => {
-		release.classList.toggle('flip', current === index)
-		release.style.order = current > index
-			? length + index - current
-			: index - current
-	})
+	requestAnimationFrame(() => {
+		releases.forEach((release, index) => {
+			release.classList.toggle('flip', current === index)
+			release.style.order = current > index
+				? length + index - current
+				: index - current
+		})
 
-	container.style.transform = `translateY(-${releases[current].clientHeight}px)`
-	container.classList.remove('translate')
-	setTimeout(() => container.classList.add('translate'), 0)
-	setTimeout(() => animate(current === 0 ? last : current - 1), 8000)
+		container.style.transform = `translateY(-${releases[current].clientHeight}px)`
+		container.classList.remove('translate')
+		requestAnimationFrame(() => container.classList.add('translate'))
+		setTimeout(() => animate(current === 0 ? last : current - 1), 10000)
+	})
 }
 
-setTimeout(() => animate(), 2000)
+setTimeout(animate, 2000)
