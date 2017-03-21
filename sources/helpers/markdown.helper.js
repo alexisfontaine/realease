6cfdca96186b8c139c8ca93e56ac267b29e24541
alexisfontaine <alexis.fontaine@isep.fr>
2017-03-21 22:58:01 +0100

    Support keyword emojis in Markdown
    Move dependencies to development

--- sources/helpers/markdown.helper.js
+++ sources/helpers/markdown.helper.js
@@ -1,5 +1,7 @@
 const marked	= require('marked')
 const highlight	= require('highlight.js')
+const keywords  = require('emojis-keywords')
+const emojis    = require('emojis-list')
 
 
 highlight.configure({
@@ -11,4 +13,9 @@ marked.setOptions({
 	highlight: (code, lang) => lang ? highlight.highlight(lang, code, true).value : code
 })
 
-module.exports = marked
+module.exports = markdown => marked(markdown)
+	.replace(/:([a-z0-9_-]+:‍?:)*[a-z0-9_-]+(:‍?(♀️|♂️|⚕️)?)/g, keyword => {
+		const index = keywords.indexOf(keyword)
+
+		return index === -1 ? keyword : emojis[index]
+	})
