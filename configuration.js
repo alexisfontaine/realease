e8bfab54346264da4dfb0bd2977e664612fe812d
alexisfontaine <alexis.fontaine@outlook.com>
2018-02-26 04:24:53 +0100

    Add new repositories

--- configuration.js
+++ configuration.js
@@ -1,17 +1,21 @@
 module.exports = {
 	repositories: [
-		'MDEwOlJlcG9zaXRvcnk5Mzg0MjY3',		// electron/electron
-		'MDEwOlJlcG9zaXRvcnkyNDU2MDMwNw==',	// babel/babel
-		'MDEwOlJlcG9zaXRvcnkxMTczMDM0Mg==',	// vuejs/vue
-		'MDEwOlJlcG9zaXRvcnkzNjc4NzMx',		// webpack/webpack
-		'MDEwOlJlcG9zaXRvcnk2MjM2NzU1OA==',	// zeit/hyper
-		'MDEwOlJlcG9zaXRvcnkyMDkyOTAyNQ==',	// Microsoft/TypeScript
-		'MDEwOlJlcG9zaXRvcnkzMjEyNzg=',		// npm/npm
-		'MDEwOlJlcG9zaXRvcnk3NjkxNjMx',		// moby/moby
 		'MDEwOlJlcG9zaXRvcnkzMjI4NTA1',		// atom/atom
 		'MDEwOlJlcG9zaXRvcnk4NzY4NDA2',		// postcss/autoprefixer
-		'MDEwOlJlcG9zaXRvcnkxMDI3MDI1MA==',	// facebook/react
+		'MDEwOlJlcG9zaXRvcnkyNjgyMDc5OA==',	// avajs/ava
+		'MDEwOlJlcG9zaXRvcnkyNDU2MDMwNw==',	// babel/babel
+		'MDEwOlJlcG9zaXRvcnk5Mzg0MjY3',		// electron/electron
 		'MDEwOlJlcG9zaXRvcnkyNTg4MDg5MQ==',	// facebook/flow
-		'MDEwOlJlcG9zaXRvcnkyNzE5Mzc3OQ=='	// Node.js
+		'MDEwOlJlcG9zaXRvcnk2MjM2NzU1OA==',	// zeit/hyper
+		'MDEwOlJlcG9zaXRvcnk0NzM5NDc3Ng==',	// lerna/lerna
+		'MDEwOlJlcG9zaXRvcnk3NjkxNjMx',		// moby/moby
+		'MDEwOlJlcG9zaXRvcnkyNzE5Mzc3OQ==',	// nodejs/node
+		'MDEwOlJlcG9zaXRvcnkzMjEyNzg=',		// npm/npm
+		'MDEwOlJlcG9zaXRvcnkxMDI3MDI1MA==',	// facebook/react
+		'MDEwOlJlcG9zaXRvcnkyMjI4Mjc4Ng==',	// sintaxi/surge
+		'MDEwOlJlcG9zaXRvcnkyMDkyOTAyNQ==',	// Microsoft/TypeScript
+		'MDEwOlJlcG9zaXRvcnkxMTczMDM0Mg==',	// vuejs/vue
+		'MDEwOlJlcG9zaXRvcnkzNjc4NzMx',		// webpack/webpack
+		'MDEwOlJlcG9zaXRvcnkxODA2ODU0Mg==',	// xtermjs/xterm.js
 	]
 }
